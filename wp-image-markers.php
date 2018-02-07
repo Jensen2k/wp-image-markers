47f28182d9de1bc7f8981b62ebd507c3ff9ca03b
vutuansw <vutuan.sw@gmail.com>
2018-02-07 10:40:42 +0700

    Update plugin url

--- wp-image-markers.php
+++ wp-image-markers.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:     WP Image Makers - Easy Hotspot Solution
- * Plugin URI:      https://wordpress.org/plugins/wp-image-markers
+ * Plugin URI:      https://wordpress.org/plugins/wp-image-makers-easy-hotspot-solution/
  * Description:     Add and drag marker icons with tooltips in a image.    
  * Version:         1.0.0   
  * Author:          WeDesignWeBuild
